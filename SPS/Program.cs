fa601b2853dfc7ae20ed9b7e355abb882c4f0a3f
Andrii99H <andrii99havryliuk@gmail.com>
2024-03-17 18:31:57 +0200

    Create authorization

--- SPS/Program.cs
+++ SPS/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace SPS
+﻿using System;
+
+namespace SPS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string nickname;
+            int age = 0;
+
+
+            Console.WriteLine("\t\tHello Player!");
+
+            while (true) // Authorization
+            {
+                Console.Write("Enter your nickname:");
+                nickname = Console.ReadLine();
+                if (string.IsNullOrEmpty(nickname))
+                {
+                    Console.WriteLine("Wrong input! Try again? (y - yes/other сharacter(s) - no)");
+                    if (Console.ReadLine() == "y")
+                        continue;
+                    else break;
+                }
+            
+                Console.Write("Enter your age:");
+                if (!int.TryParse(Console.ReadLine(), out age))
+                {
+                    Console.WriteLine("Wrong input! Try again? (y - yes/other сharacter(s) - no)");
+                    if (Console.ReadLine() == "y")
+                        continue;
+                    else break;
+                }
+                break;
+            }
+
+
+            Console.WriteLine(nickname);
+            Console.WriteLine(age.ToString());
+
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Goodbye!");
+            Console.ReadKey();
         }
     }
 }
